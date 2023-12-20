23f37c49ea946f9cbeed29494e2d7d9e47b322e4
Johannes Link <business@johanneslink.net>
2023-12-20 12:31:29 +0100

    GenerationTests use new generator.generate()

--- src/test/java/jqwik2/GenerationTests.java
+++ src/test/java/jqwik2/GenerationTests.java
@@ -11,34 +11,26 @@ class GenerationTests {
 	@Example
 	void smallInts() {
 		Generator<Integer> gen0to10 = new IntegerGenerator(-10, 100);
-
 		RandomGenSource source = new RandomGenSource("42");
 
 		for (int i = 0; i < 10; i++) {
-			Shrinkable<Integer> shrinkable = gen0to10.generate_OLD(source);
-			assertThat(shrinkable).isNotNull();
-			System.out.println("value=" + shrinkable.value());
-
-			Integer regenerated = shrinkable.regenerate();
-			System.out.println("regenerated=" + regenerated);
-
-			assertThat(regenerated).isEqualTo(shrinkable.value());
+			Integer value = gen0to10.generate(source);
+			assertThat(value).isNotNull();
+			System.out.println("value=" + value);
 		}
 	}
 
 	@Example
 	void smallIntsWithRecorder() {
 		Generator<Integer> gen0to10 = new IntegerGenerator(-100, 100);
-
 		RandomGenSource source = new RandomGenSource("42");
 
 		for (int i = 0; i < 10; i++) {
 			GenRecorder recorder = new GenRecorder(source);
-			Shrinkable<Integer> shrinkable = gen0to10.generate_OLD(recorder);
-			assertThat(shrinkable).isNotNull();
-			System.out.println("value=" + shrinkable.value());
-			System.out.println("recorded               = " + recorder.recording());
-			System.out.println("recorded by shrinkable = " + shrinkable.recording());
+			Integer value = gen0to10.generate(recorder);
+			assertThat(value).isNotNull();
+			System.out.println("value=    " + value);
+			System.out.println("recorded= " + recorder.recording());
 		}
 	}
 
@@ -49,9 +41,9 @@ class GenerationTests {
 		GenSource maxValueSource = new RecordedSource(new AtomRecording(Integer.MAX_VALUE, 0));
 		GenSource minValueSource = new RecordedSource(new AtomRecording(Integer.MAX_VALUE, 2));
 
-		assertThat(allInts.generate_OLD(maxValueSource).value())
+		assertThat(allInts.generate(maxValueSource))
 			.isEqualTo(Integer.MAX_VALUE);
-		assertThat(allInts.generate_OLD(minValueSource).value())
+		assertThat(allInts.generate(minValueSource))
 			.isEqualTo(Integer.MIN_VALUE);
 	}
 
@@ -63,14 +55,8 @@ class GenerationTests {
 		RandomGenSource source = new RandomGenSource("42");
 
 		for (int i = 0; i < 10; i++) {
-			Shrinkable<List<Integer>> shrinkable = listOfInts.generate_OLD(source);
-			System.out.println("value=" + shrinkable.value());
-			System.out.println("recording=" + shrinkable.recording());
-
-			List<Integer> regenerated = shrinkable.regenerate();
-			System.out.println("regenerated=" + regenerated);
-
-			assertThat(regenerated).isEqualTo(shrinkable.value());
+			List<Integer> value = listOfInts.generate(source);
+			System.out.println("value=" + value);
 		}
 	}
 
@@ -83,11 +69,10 @@ class GenerationTests {
 
 		for (int i = 0; i < 10; i++) {
 			GenRecorder recorder = new GenRecorder(source);
-			Shrinkable<List<Integer>> shrinkable = listOfInts.generate_OLD(recorder);
+			List<Integer> shrinkable = listOfInts.generate(recorder);
 			assertThat(shrinkable).isNotNull();
-			System.out.println("value=" + shrinkable.value());
-			System.out.println("recorded               = " + recorder.recording());
-			System.out.println("recorded by shrinkable = " + shrinkable.recording());
+			System.out.println("value=    " + shrinkable);
+			System.out.println("recorded= " + recorder.recording());
 		}
 	}
 }
