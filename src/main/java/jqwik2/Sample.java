df8f5b3ac8b9aad063388f92820997f74d536929
Johannes Link <business@johanneslink.net>
2023-12-21 17:54:57 +0100

    Implemented compareTo for Sample

--- src/main/java/jqwik2/Sample.java
+++ src/main/java/jqwik2/Sample.java
@@ -15,7 +15,6 @@ public record Sample(List<Shrinkable<Object>> shrinkables) implements Comparable
 		return shrinkables().stream().map(Shrinkable::regenerate).toList();
 	}
 
-
 	public Stream<Sample> shrink() {
 		if (size() != 1) {
 			throw new IllegalArgumentException("Only one parameter supported for now!");
@@ -25,11 +24,30 @@ public record Sample(List<Shrinkable<Object>> shrinkables) implements Comparable
 	}
 
 	@Override
-	public int compareTo(Sample o) {
-		if (size() != 1) {
-			throw new IllegalArgumentException("Only one parameter supported for now!");
+	public int compareTo(Sample other) {
+		List<Shrinkable<Object>> mySortedShrinkables = new ArrayList<>(shrinkables);
+		Collections.sort(mySortedShrinkables);
+		List<Shrinkable<Object>> otherSortedShrinkables = new ArrayList<>(other.shrinkables);
+		Collections.sort(otherSortedShrinkables);
+		int sortedChildrenComparison = compareShrinkables(mySortedShrinkables, otherSortedShrinkables);
+		if (sortedChildrenComparison != 0) {
+			return sortedChildrenComparison;
 		}
-		return shrinkables().getFirst().compareTo(o.shrinkables().getFirst());
+		return compareShrinkables(shrinkables, other.shrinkables);
+	}
+
+	private int compareShrinkables(List<Shrinkable<Object>> left, List<Shrinkable<Object>> right) {
+		int sizeComparison = Integer.compare(left.size(), right.size());
+		if (sizeComparison != 0) {
+			return sizeComparison;
+		}
+		for (int i = 0; i < left.size(); i++) {
+			int childComparison = left.get(i).compareTo(right.get(i));
+			if (childComparison != 0) {
+				return childComparison;
+			}
+		}
+		return 0;
 	}
 
 	@Override
