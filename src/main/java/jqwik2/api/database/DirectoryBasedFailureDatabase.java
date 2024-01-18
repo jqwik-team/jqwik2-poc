ebffc0c3db4006ab1342550771682b8e256494d8
Johannes Link <business@johanneslink.net>
2024-01-18 08:31:58 +0100

    FailureDatabase can save and load failing samples

--- src/main/java/jqwik2/api/database/DirectoryBasedFailureDatabase.java
+++ src/main/java/jqwik2/api/database/DirectoryBasedFailureDatabase.java
@@ -3,11 +3,13 @@ package jqwik2.api.database;
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.*;
 
 import jqwik2.api.recording.*;
 import jqwik2.api.support.*;
 
 public class DirectoryBasedFailureDatabase implements FailureDatabase {
+	public static final String SAMPLEFILE_PREFIX = "sample-";
 	private final Path databasePath;
 
 	public DirectoryBasedFailureDatabase(Path databasePath) {
@@ -20,6 +22,9 @@ public class DirectoryBasedFailureDatabase implements FailureDatabase {
 			try {
 				Files.createDirectories(databasePath);
 				System.out.println("Created database directory " + databasePath);
+				if (!databasePath.toFile().canWrite()) {
+					throw new IOException("Cannot write to " + databasePath);
+				}
 			} catch (IOException e) {
 				ExceptionSupport.throwAsUnchecked(e);
 			}
@@ -29,29 +34,65 @@ public class DirectoryBasedFailureDatabase implements FailureDatabase {
 	@Override
 	public void saveFailure(String id, SampleRecording recording) {
 		try {
-			var pathNormalizedId = id;
-			Path failureDirectory = databasePath.resolve(pathNormalizedId);
-			if (Files.notExists(failureDirectory)) {
-				Files.createDirectories(failureDirectory);
-			}
-			try {
-				var sampleId = recording.hashCode();
-				Path samplePath = failureDirectory.resolve("sample-" + sampleId);
-				if (Files.notExists(samplePath)) {
-					var sampleFile = Files.createFile(samplePath);
-					try (var writer = Files.newBufferedWriter(sampleFile)) {
-						try {
-							writer.write(recording.serialize());
-						} catch (IOException e) {
-							ExceptionSupport.throwAsUnchecked(e);
-						}
+			var propertyDirectory = propertyDirectory(id, true);
+			var samplePath = samplePath(recording, propertyDirectory);
+			if (Files.notExists(samplePath)) {
+				var sampleFile = Files.createFile(samplePath);
+				try (var writer = Files.newBufferedWriter(sampleFile)) {
+					try {
+						writer.write(recording.serialize());
+					} catch (IOException e) {
+						ExceptionSupport.throwAsUnchecked(e);
 					}
 				}
-			} catch (Exception e) {
-				ExceptionSupport.throwAsUnchecked(e);
 			}
 		} catch (IOException e) {
 			ExceptionSupport.throwAsUnchecked(e);
+		}
+	}
+
+	private static Path samplePath(SampleRecording recording, Path propertyDirectory) {
+		var sampleId = recording.hashCode();
+		return propertyDirectory.resolve("sample-" + sampleId);
+	}
+
+	private Path propertyDirectory(String id, boolean createIfNecessary) throws IOException {
+		var idBasedFileName = toFileName(id);
+		Path propertyDirectory = databasePath.resolve(idBasedFileName);
+		if (createIfNecessary && Files.notExists(propertyDirectory)) {
+			Files.createDirectories(propertyDirectory);
+		}
+		return propertyDirectory;
+	}
+
+	private String toFileName(String id) {
+		// Replace spaces with underscores. Remove all other non-alphanumeric characters.
+		return id.replaceAll("\\s", "_").replaceAll("[^a-zA-Z0-9_]", "");
+	}
+
+	@Override
+	public Set<SampleRecording> loadFailures(String propertyId) {
+		try {
+			var propertyDirectory = propertyDirectory(propertyId, false);
+			if (Files.notExists(propertyDirectory)) {
+				return Collections.emptySet();
+			}
+			return Files.list(propertyDirectory)
+						.filter(Files::isRegularFile)
+						.filter(path -> path.getFileName().toString().startsWith(SAMPLEFILE_PREFIX))
+						.toList().stream().map(this::loadSample).collect(Collectors.toSet());
+		} catch (IOException e) {
+			return ExceptionSupport.throwAsUnchecked(e);
+		}
+	}
+
+	private SampleRecording loadSample(Path path) {
+		try {
+			var lines = Files.readAllLines(path);
+			var serialized = String.join("", lines);
+			return SampleRecording.deserialize(serialized);
+		} catch (IOException e) {
+			return ExceptionSupport.throwAsUnchecked(e);
 		}
 	}
 
@@ -66,12 +107,18 @@ public class DirectoryBasedFailureDatabase implements FailureDatabase {
 	}
 
 	@Override
-	public Set<SampleRecording> loadFailures(String propertyId) {
-		return Collections.emptySet();
+	public void clear() {
+		try {
+			deleteAllIn(databasePath);
+		} catch (IOException e) {
+			ExceptionSupport.throwAsUnchecked(e);
+		}
 	}
 
-	@Override
-	public void clear() {
-
+	private void deleteAllIn(Path path) throws IOException {
+		Files.walk(path)
+			 .sorted(Comparator.reverseOrder())
+			 .map(Path::toFile)
+			 .forEach(File::delete);
 	}
 }
--- src/test/java/jqwik2/FailureDatabaseTests.java
+++ src/test/java/jqwik2/FailureDatabaseTests.java
@@ -26,30 +26,13 @@ class FailureDatabaseTests {
 
 	@AfterExample
 	void deleteDatabase() throws IOException {
-		Files.list(basePath).forEach(failureDirectory -> {
-			try {
-				System.out.println("> " + failureDirectory.getFileName() + " " + Files.isDirectory(failureDirectory));
-				Files.list(failureDirectory).forEach(samplePath -> {
-					System.out.println(">> " + samplePath.getFileName());
-					try {
-						Files.lines(samplePath).forEach(System.out::println);
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				});
-				// Files.deleteIfExists(failureDirectory);
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		});
-
-		System.out.println(basePath);
-		// Files.deleteIfExists(basePath);
+		database.clear();
+		// System.out.println(basePath);
+		Files.deleteIfExists(basePath);
 	}
 
 	@Example
 	void saveFailures() {
-
 		var sample1 = new SampleRecording(List.of(Recording.atom(1), Recording.atom(2)));
 		var sample2 = new SampleRecording(List.of(Recording.atom(1), Recording.atom(3)));
 		var sample3 = new SampleRecording(List.of(Recording.atom(1), Recording.atom(2), Recording.atom(3)));
@@ -63,7 +46,7 @@ class FailureDatabaseTests {
 
 		Set<SampleRecording> failures = database.loadFailures("id1");
 
-		// assertThat(failures).hasSize(4);
-		// assertThat(failures).contains(sample1, sample2, sample3, sample4);
+		assertThat(failures).hasSize(4);
+		assertThat(failures).contains(sample1, sample2, sample3, sample4);
 	}
 }
