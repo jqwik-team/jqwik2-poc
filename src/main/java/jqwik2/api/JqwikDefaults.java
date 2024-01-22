9420addd144ffcf4d008faf1295fe9cb2a0dadbb
Johannes Link <business@johanneslink.net>
2024-01-22 11:56:44 +0100

    If default failure database cannot be created a null database is used instead

--- src/main/java/jqwik2/api/JqwikDefaults.java
+++ src/main/java/jqwik2/api/JqwikDefaults.java
@@ -7,6 +7,8 @@ import jqwik2.api.database.*;
 
 public class JqwikDefaults {
 
+	private static FailureDatabase defaultFailureDatabase;
+
 	public static int defaultMaxTries() {
 		return 100;
 	}
@@ -28,6 +30,14 @@ public class JqwikDefaults {
 	}
 
 	public static FailureDatabase defaultFailureDatabase() {
-		return new DirectoryBasedFailureDatabase(Path.of(".jqwik", "failures"));
+		if (defaultFailureDatabase == null) {
+			try {
+				defaultFailureDatabase = new DirectoryBasedFailureDatabase(Path.of(".jqwik", "failures"));
+			} catch (Exception e) {
+				System.err.printf("Could not create default failure database: %s%n", e.getMessage());
+				defaultFailureDatabase = FailureDatabase.NULL;
+			}
+		}
+		return defaultFailureDatabase;
 	}
 }
--- src/main/java/jqwik2/api/database/FailureDatabase.java
+++ src/main/java/jqwik2/api/database/FailureDatabase.java
@@ -6,6 +6,8 @@ import jqwik2.api.recording.*;
 
 public interface FailureDatabase {
 
+	FailureDatabase NULL = new NullFailureDatabase();
+
 	void saveFailingSample(String propertyId, SampleRecording recording);
 
 	void deleteFailure(String propertyId, SampleRecording recording);
@@ -30,6 +32,43 @@ public interface FailureDatabase {
 		saveSeed(propertyId, seed);
 		for (SampleRecording sample : failingSamples) {
 			saveFailingSample(propertyId, sample);
+		}
+	}
+
+	class NullFailureDatabase implements FailureDatabase {
+		@Override
+		public void saveFailingSample(String propertyId, SampleRecording recording) {
+		}
+
+		@Override
+		public void deleteFailure(String propertyId, SampleRecording recording) {
+		}
+
+		@Override
+		public void deleteProperty(String propertyId) {
+		}
+
+		@Override
+		public Set<SampleRecording> loadFailingSamples(String propertyId) {
+			return Collections.emptySet();
+		}
+
+		@Override
+		public void clear() {
+		}
+
+		@Override
+		public Set<String> failingProperties() {
+			return Collections.emptySet();
+		}
+
+		@Override
+		public void saveSeed(String propertyId, String seed) {
+		}
+
+		@Override
+		public Optional<String> loadSeed(String propertyId) {
+			return Optional.empty();
 		}
 	}
 }
