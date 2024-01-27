6a9e3fb971390521e0cbc899280327b742d0c1fe
Johannes Link <business@johanneslink.net>
2024-01-27 17:46:51 +0100

    Fulfilling StatefulTests.transformersAreCorrectlyReported()

--- src/test/java/jqwik2/StatefulTests.java
+++ src/test/java/jqwik2/StatefulTests.java
@@ -3,6 +3,7 @@ package jqwik2;
 import java.util.*;
 
 import jqwik2.api.Arbitrary;
+import jqwik2.api.arbitraries.*;
 import jqwik2.api.stateful.*;
 
 import net.jqwik.api.*;
@@ -26,6 +27,22 @@ class StatefulTests {
 		assertThat(chain.transformations()).containsExactly(
 			"+1", "+1", "+1", "+1", "+1", "+1", "+1", "+1", "+1", "+1"
 		);
+	}
+
+	@Example
+	void transformersAreCorrectlyReported() {
+		Transformer<Integer> transformer = i -> i + 1;
+		Arbitrary<Chain<Integer>> chains =
+			Chain.startWith(() -> 0)
+				 .withTransformation(ignore -> just(transformer))
+				 .withMaxTransformations(10);
+
+		Chain<Integer> chain = chains.sample();
+
+		assertThat(collectAllValues(chain)).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+		assertThat(chain.transformers().size()).isEqualTo(10);
+		chain.transformers().forEach(t -> assertThat(t).isSameAs(transformer));
 	}
 
 	private <T> List<T> collectAllValues(Chain<T> chain) {
