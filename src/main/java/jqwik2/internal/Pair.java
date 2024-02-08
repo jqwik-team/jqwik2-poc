dffd66fd440816ac3d5f2d8e51ec3eaad1236def
Johannes Link <business@johanneslink.net>
2024-02-08 08:43:46 +0100

    Introduce Pair.of(..)

--- src/main/java/jqwik2/internal/Pair.java
+++ src/main/java/jqwik2/internal/Pair.java
@@ -1,4 +1,8 @@
 package jqwik2.internal;
 
 public record Pair<T1, T2> (T1 first, T2 second) {
+
+	public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second) {
+		return new Pair<>(first, second);
+	}
 }
