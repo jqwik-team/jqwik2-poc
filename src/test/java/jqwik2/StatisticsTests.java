040abda55884bb50c8d01c6d0517f317e55546d2
Johannes Link <business@johanneslink.net>
2024-02-18 12:08:14 +0100

    Implemented Classifier for statistical coverage checking

--- src/test/java/jqwik2/StatisticsTests.java
+++ src/test/java/jqwik2/StatisticsTests.java
@@ -1,11 +1,16 @@
 package jqwik2;
 
 import java.time.*;
+import java.util.*;
 import java.util.concurrent.atomic.*;
+import java.util.function.*;
+import java.util.stream.*;
 
 import jqwik2.api.*;
 import jqwik2.api.arbitraries.*;
 import jqwik2.api.statistics.*;
+import jqwik2.internal.*;
+import jqwik2.internal.generators.*;
 
 import net.jqwik.api.*;
 
@@ -41,6 +46,7 @@ class StatisticsTests {
 	}
 
 	@Example
+	@Disabled
 	void statisticalCheckSuccessful() {
 		var property = new JqwikProperty()
 						   .withGeneration(RANDOMIZED)
@@ -62,10 +68,11 @@ class StatisticsTests {
 			Statistics.check("Even number occurs at least 48%", n -> countEven.get() / n > 0.48)
 		);
 		System.out.println(countEven.get() + " of " + result.countChecks());
-		assertThat(result.isSuccessful()).isTrue();
+		// assertThat(result.isSuccessful()).isTrue();
 	}
 
 	@Example
+	@Disabled
 	void statisticalCheckFailed() {
 		var property = new JqwikProperty()
 						   .withGeneration(RANDOMIZED)
@@ -90,4 +97,227 @@ class StatisticsTests {
 		assertThat(result.isFailed()).isTrue();
 	}
 
+	@Example
+	void classificationExamples() {
+		var property = new JqwikProperty();
+
+		// property.forAll(Numbers.integers()).classify(
+		// 	caseOf("Even number", percentage(40), i -> i % 2 == 0).verify(i -> i % 2 == 0),
+		// 	caseOf("Odd number", percentage(40), i -> i % 2 != 0).verify(i -> i % 2 != 0)
+		// );
+
+		// int i = 100;
+		// classify(
+		// 	caseOf("Even number", i -> i % 2 == 0, () -> {
+		//
+		// 	}),
+		// )
+	}
+
+	@Example
+	void classifierAccept() {
+		var classifier = new Classifier();
+		classifier.addCase("Case 1", 40.0, args -> (int) args.get(0) >= 10);
+		classifier.addCase("Case 2", 40.0, args -> (int) args.get(0) <= -10);
+		classifier.addCase("Default", 5.0, args -> true);
+
+		Generator<Integer> integers = BaseGenerators.integers(-100, 100);
+		GenSource source = new RandomGenSource();
+
+		while (classifier.checkCoverage(1.0) == Classifier.CoverageCheck.UNSTABLE) {
+			classifier.classify(List.of(integers.generate(source)));
+		}
+
+		System.out.println();
+		System.out.println(classifier.total());
+		// System.out.println(classifier.counts());
+		System.out.println(classifier.percentages());
+		// System.out.println(classifier.deviations());
+		System.out.println(classifier.checkCoverage(1.0));
+
+		assertThat(classifier.checkCoverage(1.0)).isEqualTo(Classifier.CoverageCheck.ACCEPT);
+	}
+
+	@Example
+	void tightAccept() {
+		var classifier = new Classifier();
+		classifier.addCase("even", 49.5, args -> (int) args.get(0) % 2 == 0);
+		classifier.addCase("odd", 49.5, args -> (int) args.get(0) % 2 != 0);
+
+		Generator<Integer> integers = BaseGenerators.integers(1, 100);
+		GenSource source = new RandomGenSource();
+
+		while (classifier.checkCoverage(3.0) == Classifier.CoverageCheck.UNSTABLE) {
+			classifier.classify(List.of(integers.generate(source)));
+		}
+
+		System.out.println();
+		System.out.println(classifier.total());
+		// System.out.println(classifier.counts());
+		System.out.println(classifier.percentages());
+		System.out.println(classifier.deviations());
+		System.out.println(classifier.checkCoverage(3.0));
+
+		assertThat(classifier.checkCoverage(3.0)).isEqualTo(Classifier.CoverageCheck.ACCEPT);
+	}
+
+	@Example
+	void classifierReject() {
+		var classifier = new Classifier();
+		classifier.addCase("Case 1", 42.0, args -> (int) args.get(0) >= 10);
+		classifier.addCase("Case 2", 42.0, args -> (int) args.get(0) <= -10);
+		classifier.addCase("Default", 12.0, args -> true);
+
+		Generator<Integer> integers = BaseGenerators.integers(-100, 100);
+		GenSource source = new RandomGenSource();
+
+		while (classifier.checkCoverage(1.0) == Classifier.CoverageCheck.UNSTABLE) {
+			classifier.classify(List.of(integers.generate(source)));
+		}
+
+		System.out.println();
+		System.out.println(classifier.total());
+		System.out.println(classifier.counts());
+		System.out.println(classifier.percentages());
+		System.out.println(classifier.deviations());
+		System.out.println(classifier.checkCoverage(1.0));
+
+		assertThat(classifier.checkCoverage(1.0)).isEqualTo(Classifier.CoverageCheck.REJECT);
+	}
+}
+
+class Classifier {
+
+	public static final int MIN_TRIES = 100;
+	private final List<Case> cases = new ArrayList<>();
+	private final Map<Case, Integer> counts = new HashMap<>();
+	private final Map<Case, Double> sumOfPercentages = new HashMap<>();
+	private final Map<Case, Double> sumOfPercentageSquares = new HashMap<>();
+	private final AtomicInteger total = new AtomicInteger(0);
+
+	void addCase(String label, double minPercentage, Predicate<List<Object>> condition) {
+		var newCase = new Case(label, minPercentage, condition);
+		cases.add(newCase);
+		counts.put(newCase, 0);
+		sumOfPercentages.put(newCase, 0.0);
+		sumOfPercentageSquares.put(newCase, 0.0);
+	}
+
+	void classify(List<Object> args) {
+		total.incrementAndGet();
+		for (Case c : cases) {
+			if (c.condition().test(args)) {
+				counts.put(c, counts.get(c) + 1);
+				updateSums();
+				updateSquares();
+				break;
+			}
+		}
+	}
+
+	private void updateSquares() {
+		cases.forEach(c -> {
+			var percentage = percentage(c);
+			double sum = sumOfPercentageSquares.get(c);
+			sumOfPercentageSquares.put(c, sum + percentage * percentage);
+		});
+	}
+
+	private void updateSums() {
+		cases.forEach(c -> {
+			var percentage = percentage(c);
+			double sum = sumOfPercentages.get(c);
+			sumOfPercentages.put(c, sum + percentage);
+		});
+	}
+
+	private double percentage(Case c) {
+		if (total.get() == 0) {
+			return 0.0;
+		}
+		return counts.get(c) / (double) total.get() * 100.0;
+	}
+
+	Map<String, Integer> counts() {
+		return counts.entrySet().stream()
+					 .collect(
+						 Collectors.toMap(
+							 e -> e.getKey().label(),
+							 Map.Entry::getValue
+						 )
+					 );
+	}
+
+	Map<String, Double> percentages() {
+		return counts.entrySet().stream()
+					 .collect(
+						 Collectors.toMap(
+							 e -> e.getKey().label(),
+							 e -> percentage(e.getKey())
+						 )
+					 );
+	}
+
+	Map<String, Double> deviations() {
+		return counts.entrySet().stream()
+					 .collect(
+						 Collectors.toMap(
+							 e -> e.getKey().label(),
+							 e -> deviation(e.getKey())
+						 )
+					 );
+	}
+
+	private double deviation(Case key) {
+		var percentage = sumOfPercentages.get(key) / total.get();
+		var percentageSquare = sumOfPercentageSquares.get(key) / total.get();
+		return Math.sqrt(percentageSquare - percentage * percentage);
+	}
+
+	enum CoverageCheck {
+		ACCEPT, REJECT, UNSTABLE
+	}
+
+	CoverageCheck checkCoverage(double maxDeviationFactor) {
+		if (total.get() < MIN_TRIES) {
+			return CoverageCheck.UNSTABLE;
+		}
+
+		var checks = cases.stream()
+						  .map(c -> checkCoverage(c, maxDeviationFactor))
+						  .toList();
+
+		if (checks.stream().anyMatch(c -> c == CoverageCheck.REJECT)) {
+			return CoverageCheck.REJECT;
+		}
+		if (checks.stream().anyMatch(c -> c == CoverageCheck.UNSTABLE)) {
+			return CoverageCheck.UNSTABLE;
+		}
+		return CoverageCheck.ACCEPT;
+	}
+
+	private CoverageCheck checkCoverage(Case c, double maxDeviationFactor) {
+		var percentage = percentage(c);
+		var minPercentage = c.minPercentage();
+		var maxDeviation = deviation(c) * maxDeviationFactor;
+		if (percentage < minPercentage) {
+			if ((minPercentage - percentage) <= maxDeviation) {
+				return CoverageCheck.UNSTABLE;
+			} else {
+				return CoverageCheck.REJECT;
+			}
+		} else {
+			if ((percentage - minPercentage) <= maxDeviation) {
+				return CoverageCheck.UNSTABLE;
+			}
+		}
+		return CoverageCheck.ACCEPT;
+	}
+
+	int total() {
+		return total.get();
+	}
+}
+
+record Case(String label, double minPercentage, Predicate<List<Object>> condition) {
 }
