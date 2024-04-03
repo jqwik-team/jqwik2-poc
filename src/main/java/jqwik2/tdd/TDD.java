a23b2aead9dc237c186329d6a14dee791d179c8f
Johannes Link <business@johanneslink.net>
2024-04-03 12:05:25 +0200

    Add default id creation for TDD properties

--- src/main/java/jqwik2/tdd/TDD.java
+++ src/main/java/jqwik2/tdd/TDD.java
@@ -1,9 +1,17 @@
 package jqwik2.tdd;
 
-public interface TDD {
+import jqwik2.api.*;
 
-	static TddProperty.Builder id(String id) {
+public class TDD {
+
+	private TDD() {}
+
+	public static TddProperty.Builder id(String id) {
 		return new TddPropertyBuilder(id);
+	}
+
+	public static <T1> TddProperty.P1<T1> forAll(Arbitrary<T1> arbitrary) {
+		return new TddPropertyBuilder().forAll(arbitrary);
 	}
 
 }
--- src/main/java/jqwik2/tdd/TddPropertyBuilder.java
+++ src/main/java/jqwik2/tdd/TddPropertyBuilder.java
@@ -1,9 +1,29 @@
 package jqwik2.tdd;
 
 import jqwik2.api.*;
+import jqwik2.api.description.*;
+import jqwik2.internal.description.*;
 
 class TddPropertyBuilder implements TddProperty.Builder {
 	private final String propertyId;
+
+	TddPropertyBuilder() {
+		this(defaultId());
+	}
+
+	private static String defaultId() {
+		StackTraceElement[] stackTrace = new RuntimeException().getStackTrace();
+		for (StackTraceElement element : stackTrace) {
+			if (element.getClassName().equals(TddPropertyBuilder.class.getName())) {
+				continue;
+			}
+			if (element.getClassName().equals(TDD.class.getName())) {
+				continue;
+			}
+			return element.getClassName() + "#" + element.getMethodName();
+		}
+		throw new IllegalStateException("Could not determine default id for tdd property");
+	}
 
 	TddPropertyBuilder(String propertyId) {
 		this.propertyId = propertyId;
